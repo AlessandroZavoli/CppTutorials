29c79cb36ca89c05c12f85b247aa291c6b62dedf
Arnaud Dupuis <arnaud.dps@hotmail.fr>
2022-03-28 12:42:18 +0200

    1st upload of Shooting function method - lots of errors to fix

---
+++ CMakeFiles/rocket.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rocket.dir/src/main.cpp.o"
+  "CMakeFiles/rocket.dir/src/main.cpp.o.d"
+  "rocket"
+  "rocket.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rocket.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
